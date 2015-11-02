f60804f1760cf96b8f8042790ad5be928e52afcc
Koen Gabriëls <koeng@appsaloon.be>
2015-11-02 11:14:10 +0100

    Adds minimal plugin file

---
+++ arpu-github-test.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * ARPU Test Project for GitHub
+ *
+ * Testing plugin for ARPU for GitHub
+ *
+ * @package   AS_ARPU_TEST_PROJECT_Git_Hub
+ * @author    G
+ * @license   proprietary
+ * @link      http://appsaloon.be
+ * @copyright 2015 AppSaloon BVBA
+ *
+ * @wordpress-plugin
+ * Plugin Name:       ARPU Test Project for GitHub
+ * Description:       Plugin used to test the updating functionality of the ARPU plugin (during development)
+ * Version:           1.0.0
+ * Author:            AppSaloon
+ * Author URI:        http://www.appsaloon.be
+ */
