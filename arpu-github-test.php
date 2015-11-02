a7e637214a0367cc7e44a378bb3a311c1df33715
Koen Gabriëls <koeng@appsaloon.be>
2015-11-02 15:07:54 +0100

    adds arpu support"

--- arpu-github-test.php
+++ arpu-github-test.php
@@ -13,9 +13,20 @@
  * @wordpress-plugin
  * Plugin Name:       ARPU Test Project for GitHub
  * Description:       Plugin used to test the updating functionality of the ARPU plugin (during development)
- * Version:           1.0.1
+ * Version:           1.0.0
  * Author:            AppSaloon
  * Author URI:        http://www.appsaloon.be
  */
 
-// Just some modification to trigger/justify release test
+add_filter( 'arpu_github_plugins', 'arpu_github_test_add_to_github_plugins' );
+
+function arpu_github_test_add_to_github_plugins( $github_plugins ) {
+    $github_plugins[] = array(
+        'plugin_file' => __FILE__,
+        'github_owner' => 'AppSaloon',
+        'github_project_name' => 'arpu-test-project',
+        'access_token' => '27dfbbcf0b78579865f6aa0e6530014b71bb0e77'
+    );
+
+    return $github_plugins;
+}
